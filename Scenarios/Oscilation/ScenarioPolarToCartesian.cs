ca51da8c7681e4a00da0290cead2c99602a84302
Rick Marina <rickmarina@gmail.com>
2024-04-23 14:56:18 +0200

    remove unnecessary imports

--- Scenarios/Oscilation/ScenarioPolarToCartesian.cs
+++ Scenarios/Oscilation/ScenarioPolarToCartesian.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Shapes;
-using System.Windows;
 
 
 namespace natureofcode_wpf.Scenarios.Oscilation
